74cdc0cfc78884c9951f5cdd537f02a5b8c16a6a
devdahmer99 <eduardodahmer99@gmail.com>
2022-07-26 23:01:04 -0300

    - Modificação no Template - Finalização da primeira parte do front

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Models\Page;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $front_menu = [
+            '/' => 'Home'
+        ];
+
+        $pages = Page::all();
+        foreach($pages as $page) {
+            $front_menu[ $page['slug'] ] = $page['title'];
+        }
+
+        View::share('front_menu', $front_menu);
     }
 }
--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -37,7 +37,7 @@
                 <div class="row align-items-center">
                     <div class="col-xl-3 col-lg-2">
                         <div class="logo">
-                            <a href="index.html">
+                            <a href="/">
                                 <img src="{{asset('assets/img/logo.png')}}" alt="">
                             </a>
                         </div>
@@ -46,31 +46,17 @@
                         <div class="main-menu  d-none d-lg-block">
                             <nav>
                                 <ul id="navigation">
-                                    <li><a class="active" href="index.html">home</a></li>
-                                    <li><a href="features.html">Features</a></li>
-                                    <li><a href="Pricing.html">Pricing</a></li>
-                                    <li><a href="#">blog <i class="ti-angle-down"></i></a>
-                                        <ul class="submenu">
-                                            <li><a href="blog.html">blog</a></li>
-                                            <li><a href="single-blog.html">single-blog</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="#">pages <i class="ti-angle-down"></i></a>
-                                        <ul class="submenu">
-                                            <li><a href="elements.html">elements</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="contact.html">Contact</a></li>
+                                    @foreach($front_menu as $menuslug => $menuitem)
+                                        <li>
+                                            <a href="{{$menuslug}}">{{$menuitem}}</a>
+                                        </li>
+                                    @endforeach
                                 </ul>
                             </nav>
                         </div>
                     </div>
                     <div class="col-xl-3 col-lg-3 d-none d-lg-block">
-                        <div class="Appointment">
-                            <div class="book_btn d-none d-lg-block">
-                                <a  href="#">Download</a>
-                            </div>
-                        </div>
+
                     </div>
                     <div class="col-12">
                         <div class="mobile_menu d-block d-lg-none"></div>
@@ -91,7 +77,7 @@
                 <div class="col-xl-4 col-md-6 col-lg-4">
                     <div class="footer_widget">
                         <div class="footer_logo">
-                            <a href="#">
+                            <a href="/">
                                 <img src="{{asset('assets/img/logo.png')}}" alt="">
                             </a>
                         </div>
@@ -124,13 +110,14 @@
                 <div class="col-xl-2 offset-xl-1 col-md-6 col-lg-3">
                     <div class="footer_widget">
                         <h3 class="footer_title">
-                            Services
+                            Menu
                         </h3>
                         <ul>
-                            <li><a href="#">Team management</a></li>
-                            <li><a href="#">Collaboration</a></li>
-                            <li><a href="#">Todo</a></li>
-                            <li><a href="#">Events</a></li>
+                            @foreach($front_menu as $menuslug => $menuitem)
+                                <li>
+                                    <a href="{{$menuslug}}">{{$menuitem}}</a>
+                                </li>
+                            @endforeach
                         </ul>
 
                     </div>
